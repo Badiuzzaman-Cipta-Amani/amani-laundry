99148dcad4f3b5d0906461c48f79c93bdfbd1377
raquis <rizalpratama10010@gmail.com>
2026-07-19 23:21:00 +0700

    feat: add robots, partytown, and theme color

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
+import partytown from "@astrojs/partytown"
+import sitemap from "@astrojs/sitemap"
+import vue from "@astrojs/vue"
+import tailwindcss from "@tailwindcss/vite"
+import robotsTxt from "astro-robots-txt"
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-
-import vue from '@astrojs/vue';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), sitemap()],
+  site: "http://localhost:4321",
+  integrations: [vue(), sitemap(), robotsTxt(), partytown()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': '/src'
-      }
-    }
+        "@": "/src",
+      },
+    },
   },
-});
+})
